28bfd0e8f3d00d460f804aa3607f7fae282156d2
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-01-07 00:28:58 -0300

    ADD: Marker for favourite

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "preact/hooks";
+import MarkerFill from "./icons/MarkerFill.tsx";
+import MarkerOutline from "./icons/MarkerOutline.tsx";
 interface Props {
   ISBN: string;
   title: string;
@@ -55,9 +57,11 @@ const AddToList = (book: Props) => {
     <div class="absolute bottom-0 right-0">
       <button
         onClick={handleAddToList}
-        class="btn bg-blue-500 rounded border-sky-100 py-1 px-3 text-lg font-semibold text-white m-2"
+        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
       >
-        {isBookAdded ? "Agregado" : "Agregar"}
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          {isBookAdded ? <MarkerFill /> : <MarkerOutline />}
+        </span>
       </button>
     </div>
   );
